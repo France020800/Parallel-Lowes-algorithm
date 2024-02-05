475099746f027914239542feca2ef4a23270d30a
France020800 <francescomarchini7@gmail.com>
2024-02-05 23:14:04 +0100

    Added tester script

---
+++ tester.py
@@ -0,0 +1,27 @@
+import subprocess
+import matplotlib.pyplot as plt
+
+def run_sequential():
+    result = subprocess.run(['python', 'Lowes_algorithm.py'], stdout=subprocess.PIPE)
+    execution_time = float(result.stdout.decode('utf-8').split('\n')[-2])
+    return execution_time
+
+def run_parallel(num_processes):
+    result = subprocess.run(['python', 'parallel_Lowes_algorithm.py', str(num_processes)], stdout=subprocess.PIPE)
+    execution_time = float(result.stdout.decode('utf-8').split('\n')[-2])
+    return execution_time
+
+speedup = []
+sequential_time = run_sequential()
+for i in range(1, 17):
+    parallel_time = run_parallel(i)
+    speedup.append(round(sequential_time / parallel_time, 4))
+    print('Speedup with {} processes: {:.4f}'.format(i, speedup[-1]))
+
+plt.plot(range(1, 17), speedup, label='Speedup', marker='o')
+plt.plot(range(1,17), range(1,17), label="Ideal", marker='o', color='coral')
+plt.xlabel('Number of processes')
+plt.ylabel('Speedup')
+plt.title('Speedup vs Number of processes')
+plt.legend()
+plt.show()
